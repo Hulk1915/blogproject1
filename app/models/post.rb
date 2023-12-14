3ff13182c8c793f92195f28a073feb5c404b2ba8
Hulk1915 <1999dim@mail.ru>
2023-12-14 12:49:12 +0300

    new

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
+
   has_many :comments
-  validates :title, presence: true, length: {minimum: 5}
-  validates :title, presence: true, length: {maximum: 100}
-  validates :body, presence: true, length: {minimum: 10}
+  validates :title, presence: true, length: { minimum: 5, maximum: 100, too_short: "слишком короткий заголовок надо (минимум %{count} символов)", too_long: "слишком длинный заголовок надо (максимум %{count} символов)" }
+  validates :body, presence: true, length: { minimum: 10, too_short: "слишком короткое описание надо (минимум %{count} символов)" }
+
 end
