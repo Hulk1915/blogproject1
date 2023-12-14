3030d5760677c95305c288c48a4a7d1f3f9d9f0f
Hulk1915 <1999dim@mail.ru>
2023-12-14 12:45:04 +0300

    new validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments
   validates :title, presence: true, length: {minimum: 5}
-  validates :title, presence: true, length: {minimum: 100}
+  validates :title, presence: true, length: {maximum: 100}
+  validates :body, presence: true, length: {minimum: 10}
 end
